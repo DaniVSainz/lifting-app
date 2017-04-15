f66a6ba87faa02199ed72571b0faa952950bc1f3
DaniVSainz <danivsainz@gmail.com>
2017-04-15 19:25:08 -0400

    Make create method in lifts controller

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -1,4 +1,15 @@
 class LiftsController < ApplicationController
+
+  def create
+    @lift = Lift.new(lift_params)
+
+    if @lift.save
+      render json: @lift
+    else
+      render json: @lift.errors, status: :unprocessable_entity
+    end
+  end
+
   def index
     @lifts = Lift.all
   end
